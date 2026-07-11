b08f70ed20d8b5a3b6bcab8ce6e738d92b8017b2
정세영 <terry40257te@gmail.com>
2026-07-11 15:07:17 +0900

    fix: integrate custom high-quality penguin character SVG into program icon and help modal title

--- magnifier.py
+++ magnifier.py
@@ -55,18 +55,87 @@ LUCIDE_CLOSE_SVG = """
 </svg>
 """
 
-# Custom Lucide-styled Penguin SVG design (stroke-width: 2.3, color: #0a84ff)
-LUCIDE_PENGUIN_SVG = """
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#0a84ff" stroke-width="2.3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-penguin">
-  <path d="M12 2a4 4 0 0 0-4 4v3a6 6 0 0 0 12 0V6a4 4 0 0 0-4-4z"/>
-  <path d="M12 8a2.5 2.5 0 0 0-2.5 2.5v3.5a2.5 2.5 0 0 0 5 0v-3.5A2.5 2.5 0 0 0 12 8z"/>
-  <path d="M6 10c-1 1-1.5 2.5-1 4 .5 1.5 2 2 3 1.5"/>
-  <path d="M18 10c1 1 1.5 2.5 1 4-.5 1.5-2 2-3 1.5"/>
-  <path d="M11 6.5l1 1 1-1-1-.5z" fill="#0a84ff"/>
-  <path d="M9 20l-1 2h3l-1-2"/>
-  <path d="M15 20l1 2h-3l1-2"/>
-</svg>
-"""
+# New high-quality original penguin character SVG provided by the user
+LUCIDE_PENGUIN_SVG = """<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 150 150">
+<!-- SVG created with Arrow, by QuiverAI (https://quiver.ai) -->
+  <style type="text/css">.cls-0 {fill:#283A60;}
+.cls-1 {fill:url(#SVGID_1_);}
+.cls-2 {fill:#4B739F;}
+.cls-3 {fill:#FFFFFF;}
+.cls-4 {fill:url(#SVGID_2_);}
+.cls-5 {fill:url(#SVGID_3_);}
+.cls-6 {fill:none;stroke:#283A60;stroke-width:2;stroke-linecap:round;stroke-miterlimit:10;}
+.cls-7 {fill:url(#SVGID_4_);}
+.cls-8 {fill:#F9D352;}
+.cls-9 {opacity:0.3;fill:url(#SVGID_5_);enable-background:new    ;}
+.cls-10 {fill:url(#SVGID_6_);}
+.cls-11 {fill:url(#SVGID_7_);}
+.cls-12 {fill:url(#SVGID_8_);}
+.cls-13 {fill:url(#SVGID_9_);}</style>
+  <path class="cls-0" d="m121.6 68.9c-2.7-3.6-2.6-16.9-5.2-26.5-4-15.1-18.4-31.9-41.3-31.9s-37 14.9-41.6 33.2c-1.6 6.2-2.2 19.8-4.1 24.6-1 2.4-7.4 8.6-11.2 20.4-3.3 10.6-2.4 17.3 2.3 17.2 2.8-0.1 5.7-2.3 7.6-3.8 0.9 10 5.5 17.4 13.6 24-2 1.3-5.6 3.9-5.6 6.7-0.1 4.1 4 4.3 8.1 3.9 2.3 2.1 3.7 2.5 7.1 2.5 4.7 0 11.1-1.8 14.4-4.9 4.3 0.4 12.2 0.5 18.5-0.1 2.4 2 8.1 4.9 15.3 5 2.7 0 4.4-0.4 6.1-2.4l0.1-0.1c3.7 0.2 8.4 1 8.3-3.8 0-2.9-3.7-5.5-5.8-6.7 2.8-1.7 7.7-6.5 9.8-10.5 1.8-3.5 3.5-9.1 3.6-13.9 2.4 1.7 4.7 4 8.1 4.1 6.8 0 5.3-14.6-2.5-29.1-0.7-1.1-3.6-5.9-5.6-7.9z"/>
+  <linearGradient id="SVGID_1_" x1="75" x2="75" y1="-94.8" y2="-27.6" gradientTransform="translate(0 104)" gradientUnits="userSpaceOnUse">
+    <stop stop-color="#4B739E" offset="0"/>
+    <stop stop-color="#4C75A1" offset=".5909"/>
+    <stop stop-color="#4B6088" offset=".9949"/>
+  </linearGradient>
+  <path class="cls-1" d="m120.3 72c-3.2-4.7-3.7-8.3-4.9-20.7-1.9-18.3-16.4-38.2-40.4-38.2-19.5 0-34.7 14.3-39.2 31.9-1.2 4.9-2.2 20-4 23.6-1.7 3.4-8.3 10.7-11.5 22.5-0.4 1.8-2.1 12 0.4 12 2.9-0.1 5.8-3 7.5-5h0.1c-0.1-6.3 2.2-13 4.2-14.3-1.6 7-3 21.6 1.9 30.1 3.9 6.6 9.1 10.7 15.1 13l3-1.4 22.4 6.6 25.3-3.4 6.1-3.8c4.2-2.7 7.8-5.8 10.9-12.9 3.9-9 2.4-20.6 0.1-28.3 2.4 1.7 4.3 7.2 4.6 14.2h-0.1c2.5 2.4 4.7 4.6 7.8 5.2 1.8 0.4 2.8-11.2-5.3-25.7-0.4-1-2.6-4-4-5.4z"/>
+  <path class="cls-2" d="m31.5 86.6c-2.4 2.5-6.5 10.3-12.2 12.2v4.2h6.2l3.5-4 2.5-12.4z"/>
+  <path class="cls-2" d="m119.6 87c1.9 4 6.1 9.9 11.1 11.8l0.2 4.9-5.5-0.7-4.5-3.6-1.3-12.4z"/>
+  <path class="cls-3" d="m102.2 75.1c2.5-2.4 7.5-7.6 7.5-16.1 0-10-5.1-23.9-16.2-23.9-8.5 0-14.3 7.5-14.3 21.8l-8.6 0.1c0-9.7-3.5-21.9-13.8-21.9-8.9 0-16.6 9.9-16.6 22.6 0 8.8 4.3 14.4 7.6 17.6-2.7 3.3-10.7 13.7-10.7 28.7 0 8 3.2 16 9.1 20.6 4.8 3.6 14.2 7.5 28.7 7.5 15.3 0 23.7-2.7 28.2-5.6 5.1-3.4 10-11.8 10-21.9 0-13.1-6.1-22.7-10.9-29.5z"/>
+  <linearGradient id="SVGID_2_" x1="50.5" x2="113.8" y1="-89.1" y2="-89.1" gradientTransform="translate(0 104)" gradientUnits="userSpaceOnUse">
+    <stop stop-color="#fff" stop-opacity=".4" offset="0"/>
+    <stop stop-color="#BCD0E2" stop-opacity=".9" offset="1"/>
+  </linearGradient>
+  <path class="cls-4" d="m75 15.1c18.5 0 34.6 12.4 38.8 28.5l-0.2-0.5c-3.5-14.9-16.6-29.8-38.6-30-13.7 0-25.3 6.9-31.9 17.8 5.8-7.4 16.6-15.8 31.9-15.8z"/>
+  <linearGradient id="SVGID_3_" x1="75" x2="75" y1="-17.8" y2="-4.144" gradientTransform="translate(0 104)" gradientUnits="userSpaceOnUse">
+    <stop stop-color="#CFDCE5" offset="0"/>
+    <stop stop-color="#CCDCE7" offset="1"/>
+  </linearGradient>
+  <path class="cls-5" d="m75 72.2c-15 0-23.5 6-30.1 14.6l-7.8 12.2c-0.1 7 2.5 21.6 10.4 26.5 6.6 4.1 16 6.4 27.4 6.4 11.8 0.2 22.1-2.2 27-5.3 6.8-3.6 10.7-12.6 11.2-19.7l-3.2-6.7c-5.3-12.2-12.2-28-34.9-28z"/>
+  <path class="cls-6" d="m65.2 63"/>
+  <linearGradient id="SVGID_4_" x1="75.05" x2="75.05" y1=".8404" y2="27.47" gradientUnits="userSpaceOnUse">
+    <stop stop-color="#FAD250" offset=".3939"/>
+    <stop stop-color="#DCAC52" offset="1"/>
+  </linearGradient>
+  <path class="cls-7" d="m75 85.8c-14.6 0-27.7 11.9-27.7 27.3s6.8 19 28.2 19c21.1 0 27.2-5.1 27.2-18.1s-10-28.2-27.7-28.2z"/>
+  <path class="cls-8" d="m44.7 127.6c-2.1 1-6.8 3.8-6 6.1 0.4 1.2 5.5 0.4 6.5 0.4 1.3 0 1.2 2.8 5.2 2.8 4.8 0.1 9.8-1.6 11.6-3-7-0.9-15.4-4.4-17.3-6.3z"/>
+  <linearGradient id="SVGID_5_" x1="41.4" x2="57.9" y1="131.7" y2="131.7" gradientUnits="userSpaceOnUse">
+    <stop stop-color="#DD8D38" offset="0"/>
+    <stop stop-color="#DB9239" offset="1"/>
+  </linearGradient>
+  <path class="cls-9" d="m41.4 129.4c2.1 0.2 12.1 6.3 15.5 6.3 0.8 0 1-0.3 1-0.3l2.3-1.7c-2.2-0.6-11.3-2.8-15.6-6l-3.2 1.7z"/>
+  <linearGradient id="SVGID_6_" x1="88.2" x2="111.2" y1="131.9" y2="131.9" gradientUnits="userSpaceOnUse">
+    <stop stop-color="#F5CA37" offset="0"/>
+    <stop stop-color="#F4BB2E" offset="1"/>
+  </linearGradient>
+  <path class="cls-10" d="m105.4 127.9c-2.5 1.6-9.3 4.6-17.2 5.8 2.2 1.4 8 3.2 12.2 3 3.3-0.1 3.5-2.6 4.2-2.8l0.4-0.1c0.7-0.1 5.6 1.2 6.2-0.2 1.2-2.2-3.5-5-5.8-5.7z"/>
+  <linearGradient id="SVGID_7_" x1="93.5" x2="107.2" y1="131.6" y2="131.6" gradientUnits="userSpaceOnUse">
+    <stop stop-color="#DE9539" offset="0"/>
+    <stop stop-color="#DF9739" offset="1"/>
+  </linearGradient>
+  <path class="cls-11" d="m105.5 127.9c-3 1.7-8.9 4.1-12 4.5l-0.6 2.9c3.1 1.1 11.5-4.2 14.3-5.8v-1.6l-1-0.2h-0.7z"/>
+  <linearGradient id="SVGID_8_" x1="74.84" x2="74.84" y1="57.6" y2="70.72" gradientUnits="userSpaceOnUse">
+    <stop stop-color="#F5CB2E" offset="0"/>
+    <stop stop-color="#F7BD28" offset=".5955"/>
+  </linearGradient>
+  <path class="cls-12" d="m74.9 56.7c-3.2 0-9.5 3.8-9.5 6.3 0 1.6 6.1 7.7 9.6 7.7 2.6 0 9.5-5.1 9.5-7.7 0-2.1-6.1-6.3-9.6-6.3z"/>
+  <linearGradient id="SVGID_9_" x1="75.01" x2="75.01" y1="65.18" y2="70.41" gradientUnits="userSpaceOnUse">
+    <stop stop-color="#E39F40" offset="0"/>
+    <stop stop-color="#DA8D38" offset="1"/>
+  </linearGradient>
+  <path class="cls-13" d="m65.6 63.9c1.1 2 6.9 6.5 9.3 6.6 2 0.2 6.2-2.6 9.5-6.5-6.2 2.7-8.7 2.4-9.2 2.4-1.7 0-7.1-1.8-9.6-2.5z"/>
+  <path class="cls-0" d="m75.1 55.7c-3.7-0.1-10.8 3.9-10.8 7s7.3 9 10.6 9.1c3.1 0.2 10.6-5.7 10.6-8.7 0.1-2.7-6.1-7.2-10.4-7.4zm-0.2 14.2c-1.5 0-5.9-2.7-8-5.2 2.2 0.8 6.2 2.2 8.1 2.2s5.7-1.3 7.6-2c-2.1 2-5.9 5-7.7 5zm0-4.4c-2 0-8.5-2.4-8.5-2.8 0-1.1 5.2-5 8.5-5 3.1-0.1 8.6 3.4 8.7 5 0.1 0.3-6.9 2.8-8.7 2.8z"/>
+  <path class="cls-0" d="m56.2 47.8"/>
+  <ellipse class="cls-0" cx="56.2" cy="54.2" rx="4.6" ry="5.9"/>
+  <circle class="cls-3" cx="57.4" cy="51.8" r="1.7"/>
+  <ellipse class="cls-0" cx="93.9" cy="54.2" rx="4.7" ry="6"/>
+  <circle class="cls-3" cx="95" cy="51.8" r="1.7"/>
+  <linearGradient id="SVGID_10_" x1="74.87" x2="74.87" y1="26.87" y2="40.01" gradientTransform="matrix(1 0 0 -1 0 152)" gradientUnits="userSpaceOnUse">
+    <stop stop-color="#AB8040" stop-opacity=".5" offset="0"/>
+    <stop stop-color="#CD9F43" offset="1"/>
+  </linearGradient>
+  <path d="m47.4 118c0.5 4.4 2 7.4 4.2 10 2.5 1 9.4 3.9 23.1 4.1 10.3 0 19.4-1.5 23.7-3.6 2-2.5 3.6-5.5 4-10.3-4.4 2.9-12.9 7.4-27.2 7.5-11.8 0-21.2-3.1-27.8-7.7z" fill="url(#SVGID_10_)"/>
+</svg>"""
 
 def get_icon_path():
     if hasattr(sys, '_MEIPASS'):
@@ -393,14 +462,14 @@ class HelpModal(QDialog):
         container_layout.setContentsMargins(24, 24, 24, 24)
         container_layout.setSpacing(16)
         
-        # Lucide Penguin Icon next to clean title label text (Replaces unicode emoji penguin)
+        # Lucide-style original penguin SVG loader next to clean title label text
         title_layout_row = QHBoxLayout()
         title_layout_row.setSpacing(8)
         title_layout_row.setAlignment(Qt.AlignmentFlag.AlignCenter)
         
         title_icon = QLabel()
-        title_icon.setFixedSize(22, 22)
-        title_icon.setPixmap(get_svg_icon(LUCIDE_PENGUIN_SVG).pixmap(22, 22))
+        title_icon.setFixedSize(24, 24)
+        title_icon.setPixmap(get_svg_icon(LUCIDE_PENGUIN_SVG).pixmap(24, 24))
         
         title_label = QLabel("펭구쫭을 위한 사용 가이드")
         title_label.setStyleSheet("font-size: 18px; font-weight: 600; color: #ffffff;")
